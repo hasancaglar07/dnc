4d619d28a1062da8a9c721983d012bbd076af00b
hasan caglar <bubutiko13@gmail.com>
2026-04-17 19:53:08 +0300

    fix: add socials property to team data type

--- dnc7-nextjs-clean/data/team.ts
+++ dnc7-nextjs-clean/data/team.ts
@@ -4,47 +4,79 @@ export const team = [
     name: 'Çağlar Şahin',
     role: 'Yazılım Yazar',
     image: '/assets/images/team/1.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 2,
     name: 'Berk Karakaya',
     role: 'Yazar Yönetir',
     image: '/assets/images/team/2.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 3,
     name: 'Ömür Dinç',
     role: 'Yönetir Çeker',
     image: '/assets/images/team/3.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 4,
     name: 'Agit Dolaşır',
     role: 'Çeker Kurar',
     image: '/assets/images/team/4.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 5,
     name: 'Erva Ceylan',
     role: 'Kurar Paylaşır',
     image: '/assets/images/team/5.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 6,
     name: 'Ufuk Akyüz',
     role: 'Paylaşır Çizer',
     image: '/assets/images/team/6.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 7,
     name: 'Mehmet Özcan',
     role: 'Çizer Sunar',
     image: '/assets/images/team/7.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
   {
     id: 8,
     name: 'Gökhan Bozkurt',
     role: 'Sunar Yazar',
     image: '/assets/images/team/8.png',
+    socials: {
+      linkedin: 'https://linkedin.com',
+      twitter: 'https://twitter.com',
+    },
   },
 ];
